45a799f888976a22e9daa26c599f572aee2845f8
Andrew O'Rourke <andreworourke84@gmail.com>
2022-05-29 22:05:46 +1000

    destination doesn't exist exception

--- src/ds/graph/DestinationDoesntExist.java
+++ src/ds/graph/DestinationDoesntExist.java
@@ -2,6 +2,11 @@ package ds.graph;
 
 public class DestinationDoesntExist extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8861377863098833260L;
+
 	public DestinationDoesntExist(String message) {
 		super(message);
 	}
---
+++ src/ds/graph/DestinationNotReachable.java
@@ -0,0 +1,9 @@
+package ds.graph;
+
+public class DestinationNotReachable extends Exception {
+
+	public DestinationNotReachable(String message) {
+		super(message);
+	}
+
+}
